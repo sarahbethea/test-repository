7d601eecdd9b75f0b3f5b2cc2b993af2380edee7
Sarah Bethea <sarahbethea@sarahs-mbp.lan>
2024-02-24 17:49:34 -0700

    first commit

---
+++ eclipse-workspace/mysql-java2/src/main/java/projects/dao/DbConnection.java
@@ -0,0 +1,38 @@
+package projects.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import projects.exception.DbException;
+
+public class DbConnection {
+	
+	private static String HOST = "localhost";
+	private static String PASSWORD = "projects";
+	private static int PORT = 3306;
+	private static String SCHEMA = "projects";
+	private static String USER = "projects";
+
+	public static Connection getConnection() {
+		String uri = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s", 
+				HOST, PORT, SCHEMA, USER, PASSWORD);
+		
+		System.out.println("Connecting with url =" + uri);
+		
+		try {
+			Connection conn = DriverManager.getConnection(uri);
+			System.out.println("Connection successful!");
+			return conn;
+		} catch (SQLException e) {
+			System.out.println("Unable to get connection at " + uri);
+			throw new DbException("Connection failed: " + e.getMessage(), e);
+		}
+		
+	
+	}
+	
+	
+	
+	
+}
